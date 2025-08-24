0cdf18e4b749e44bcee7fc81b2d9bce69c2503dd
narozhniakmarian <narozhniakmarian@gmail.com>
2025-08-24 15:44:01 +0200

    export type

--- src/task-17.tsx
+++ src/task-17.tsx
@@ -1,27 +1,27 @@
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
-}
-
+// interface User {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+//   address: {
+//     street: string;
+//     suite: string;
+//     city: string;
+//     zipcode: string;
+//     geo: {
+//       lat: string;
+//       lng: string;
+//     };
+//   };
+//   phone: string;
+//   website: string;
+//   company: {
+//     name: string;
+//     catchPhrase: string;
+//     bs: string;
+//   };
+// }
+import type { User } from "./task-18";
 const fetchUsers = async (): Promise<User[]> => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data: User[] = await response.json();
